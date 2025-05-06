fc709f4566f54aa399230027ae89aa974900cd46
Alex Iribarren <alex.iribarren@gmail.com>
2025-05-06 17:46:59 +0200

    Remove deprecated option

--- build_files/90-signing.sh
+++ build_files/90-signing.sh
@@ -36,7 +36,7 @@ jq --arg image_registry "${IMAGE_REGISTRY}" \
         }
     ] }
     + .
-    | .transports *= (["docker-daemon", "atomic", "containers-storage", "dir", "oci", "oci-archive", "docker-archive", "tarball"]
+    | .transports *= (["docker-daemon", "containers-storage", "dir", "oci", "oci-archive", "docker-archive", "tarball"]
         | map({(.): {"": [{"type": "insecureAcceptAnything"}]}})
         | add)
     | .default[0].type = "reject"' "${POLICY_FILE}" > "/tmp/POLICY.tmp"
