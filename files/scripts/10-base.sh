2207121eb501fbc3fe7e9aa9002b5aca47a978bc
Alex Iribarren <alex.iribarren@gmail.com>
2025-05-17 21:03:49 +0200

    Fix comment

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-# This is the base for a minimal GNOME system on AlmaLinux.
+# Example of how you start customizing the image
 
 dnf install -y 'dnf-command(config-manager)'
 dnf config-manager --set-enabled crb
